e824daf1c05a04f794909f752a34e1c57195ae11
Ryan Churchill <ryan.churchill@gmail.com>
2019-09-17 20:00:57 -0700

    64 PlayClipAtPoint

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (breakSound != null)
+        {
+            // Udemy uses Camera.main.transform.position
+            AudioSource.PlayClipAtPoint(breakSound, transform.position);
+        }
         Destroy(gameObject);
     }
 }
